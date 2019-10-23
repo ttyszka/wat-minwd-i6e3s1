b6baa9f0df7b71b64d688fe0bb88ae27893e2ed3
vbv13 <pjastrzebscy@gmail.com>
2019-10-23 23:05:36 +0200

    project files

---
+++ lab2/66100/index2.js
@@ -0,0 +1,57 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const ObjectsToCsv = require("objects-to-csv");
+
+const url = "https://pl.wiktionary.org/wiki/Indeks:Angielski_-_Rodzina";
+
+
+async function getFamilyData() {
+    const html = await request(url)
+
+    const familyMembers = cheerio('tr > td > a', html)
+        .map(async (index, element) => {
+        const member = element.children[0].data
+        return {member}
+    })
+    .get()
+
+    return Promise.all(familyMembers)
+}
+
+async function getDescription(membersWithHeaders) {
+    return await Promise.all(
+        membersWithHeaders.map(async member => {
+            try {
+                const htmlResult = await request.get(member.url);
+                const $ = await cheerio.load(htmlResult);
+                member.description = $('dl').text();
+                return {member};
+            } catch(error) {
+                console.error(error);
+            }
+
+        })
+    )
+}
+
+async function createCsvFile(data) {
+    console.log(typeof(data))
+    let csv = new ObjectsToCsv(data);
+  
+    // Save to file:
+    await csv.toDisk("./test.csv");
+  }
+
+async function scrapeWiki() {
+    const membersWithHeaders = await getFamilyData()
+    const membersFullData = await getDescription(membersWithHeaders)
+
+    const stringData = membersFullData.join(',')
+    const stringDataWithoutLastComma = stringData.substring(0, stringChanged.length - 1)
+    const parseMembersFullData = JSON.parse("[" + stringDataWithoutLastComma + "]")
+    //const parseMembersFullData = Array.from(stringDataWithoutLastComma)
+
+    await createCsvFile(parseMembersFullData)
+}
+
+scrapeWiki()
