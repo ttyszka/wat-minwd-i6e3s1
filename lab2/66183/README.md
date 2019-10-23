a281c9676f03cb86a6ab77d9c5fd2dc10013f5f7
Marek <marek.roskowicz@student.wat.edu.pl>
2019-10-23 12:07:29 -0700

    Updated README file, added comments, fix location info

--- lab2/66183/README.md
+++ lab2/66183/README.md
@@ -17,6 +17,9 @@
     -p                     Podaj czy interesuje cie dom czy mieszkanie
     -rt                    Podaj czy interesuje cie wynajem czy sprzedaz
     -c                     Podaj misto ktore cie interesuje
+    
+    Optional arguments:
+    
     -d                     Podaj dzielnice ktore cie interesuje
     
     Example: 
@@ -28,3 +31,19 @@
     Scrapper create dirs /img & /json collaterally to /src dir
     In /img dir scrapper saves all photos
     In /json dir scrapper saves info from Query in JSON format
+    
+#How it works
+    Scraper downloads all pages from category based on user args input
+    
+    If scrapper will not find any page the error will be thrown
+    
+    Scraper goes throught all pages in specific category en route and gets single
+    offer then gets inside to subpage of that offer and mine additional information
+    Then it goes to the next offers.
+    Page by page
+    
+    Scraper can run into problem when otodom.pl will block the queries for pages.
+    If than problem occurs scrapper waits 200 ms then it tries again.
+    
+    Json files for each page are saved in current working dircetory in subdir ./json
+    Image are saved in current working directory in subdir ./img
--- lab2/66183/src/main.py
+++ lab2/66183/src/main.py
@@ -8,42 +8,52 @@ from urllib.parse import ParseResult
 
 from scrapping import *
 
-path = os.path.dirname(os.getcwd())
-imagePath = path + '/img'
-jsonPath = path + '/json'
-mainURL = ''
-offersQuantity = 0
+path = os.path.dirname(os.getcwd())  # current working dir
+imagePath = path + '/img'  # image path
+jsonPath = path + '/json'  # json path
+mainURL = ''  # main url with domain name and path variables like: sprzedaz, wynajem
+offersQuantity = 0  # variable hold quantity of all offers
 
 
+#   setup argument parser with 4 arguments
+#
 def setup():
     parser = argparse.ArgumentParser(description='Provide input')
     parser.add_argument('-p', '--propertytype', nargs='+', required=True,
                         help='Podaj czy interesuje cie dom czy mieszkanie'
-                        , type=lambda input: checkArgsCorrectness(parser, ("dom", "mieszkanie"), input))
-    parser.add_argument('-rt', '--rentaltype', nargs='+', required=True,
+                        , type=lambda input: checkArgsCorrectness(parser, ("dom", "mieszkanie"),
+                                                                  input))  # lamda invoke checkArgsCorrecteness
+    parser.add_argument('-rt', '--rentaltype', nargs='+', required=True,  # to check if passed args are correct
                         help='Podaj czy interesuje cie wynajem czy sprzedaz',
                         type=lambda input: checkArgsCorrectness(parser, ("wynajem", "sprzedaz"), input))
     parser.add_argument('-c', '--city', nargs='+', required=True,
                         help='Podaj misto ktore cie interesuje')
-    parser.add_argument('-d', '--district', nargs='+', required=True,
+    parser.add_argument('-d', '--district', nargs='+',
                         help='Podaj dzielnice ktore cie interesuje')
 
     args = parser.parse_args()
     return vars(args)
 
 
+#   function check if arguments passed from console arr correct
+#   parser which is parser instance
+#   choices which are list of expected correct value
+#   input which is argument value
 def checkArgsCorrectness(parser, choices, input):
     if input not in choices:
         parser.error("Args doesn't end with one of {}".format(choices))
     return input
 
 
+#   function running webscrapper
+#   run whole app and download all offers from specific city and district
 def getAllOffer():
     s, offersQuantity = getAllPageOffer(mainURL)
     convertToJsonAndSave(s, 1)
 
     pageQuantity = math.ceil(offersQuantity / offersPerPage)
 
+    # function after downloading first page show statistic about offers
     print("Offers quantity: " + str(offersQuantity))
     print("Offers per page: " + str(offersPerPage))
     print("Pages quantity: " + str(pageQuantity) + "\n\n")
@@ -52,6 +62,8 @@ def getAllOffer():
 
     if (pageQuantity > 1):
         for i in range(2, pageQuantity + 1):
+            # if the HttpError 503 occurs it's mean that otodom.pl prevent from  web scrapping
+            # need to wait and download page once again then save
             try:
                 pageurl = pageURL(mainURL, i)
                 print("Downloading page: " + str(i) + " " + pageurl)
@@ -67,6 +79,11 @@ def getAllOffer():
                     time.sleep(0.2)
 
 
+#   function with 2 args:
+#       -array of Apartment object
+#       -file name
+#   function convert Apartment objects to json and save to file
+
 def convertToJsonAndSave(appartmentArray, fileName):
     with open(jsonPath + '/page' + str(fileName),
               'w') as outfile:
@@ -74,7 +91,9 @@ def convertToJsonAndSave(appartmentArray, fileName):
             json.dump(appartment.__dict__, outfile, indent=1, separators=(',', ': '))
 
 
-def createURL(rentalType='sprzedaz', propertyType='mieszkanie', city='warszawa', district='bemowo'):
+#  function created url based on category
+#   category which was chose with app args
+def createURL(rentalType='sprzedaz', propertyType='mieszkanie', city='warszawa', district=''):
     global mainURL
     parsedResult = ParseResult(scheme='https',
                                netloc=hostURL,
@@ -87,7 +106,9 @@ def createURL(rentalType='sprzedaz', propertyType='mieszkanie', city='warszawa',
     mainURL = parsedResult.geturl()
 
 
+#   function create and setup project dir like /img dir and /dir
 def createProjectDirs():
+    print("Creating dirs")
     if not os.path.exists(imagePath):
         os.mkdir(imagePath)
     if not os.path.exists(jsonPath):
@@ -96,12 +117,16 @@ def createProjectDirs():
 
 def main():
     args = setup()
+    print("Scrapper was launched")
     try:
-        createProjectDirs()
-        setImagePath(imagePath)
-        createURL(args['rentaltype'][0], args['propertytype'][0], args['city'][0], args['district'][0])
-        getAllOffer()
-    except HTTPError as httperror:
+        createProjectDirs()  # setup project dirs
+        setImagePath(imagePath)  # setup image dir in scrapping.py module
+        if not args['district'] is None:  # based on args from input create url with or without district
+            createURL(args['rentaltype'][0], args['propertytype'][0], args['city'][0], args['district'][0])
+        else:
+            createURL(args['rentaltype'][0], args['propertytype'][0], args['city'][0])
+        getAllOffer()  # run scraper
+    except HTTPError as httperror:  # if http error occur print status of http error
         print("Error occur: " + str(httperror))
     except OSError as oserror:
         print("Creation of the directory failed: " + str(oserror))
