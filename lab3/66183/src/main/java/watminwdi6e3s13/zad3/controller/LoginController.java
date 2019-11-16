91a6eca6ee8f0d6d3fce3c0ea6c2417307c59d99
Marek <marek.roskowicz@student.wat.edu.pl>
2019-11-15 18:10:09 -0800

    created login page

---
+++ lab3/66183/src/main/java/watminwdi6e3s13/zad3/controller/LoginController.java
@@ -0,0 +1,18 @@
+package watminwdi6e3s13.zad3.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+
+    @GetMapping
+    public String login() {
+
+        return "login";
+    }
+}
