807314a65a6fda0db57efe50078d989c50e4b163
Marek <marek.roskowicz@student.wat.edu.pl>
2019-11-30 01:44:33 +0100

    Done user overview page, need to fix Fetching data about hashes

--- lab3/66183/src/main/java/watminwdi6e3s13/zad3/controller/InfoController.java
+++ lab3/66183/src/main/java/watminwdi6e3s13/zad3/controller/InfoController.java
@@ -5,7 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import twitter4j.HashtagEntity;
 import twitter4j.TwitterException;
 import twitter4j.auth.AccessToken;
 import watminwdi6e3s13.zad3.pojo.TwitterUser;
@@ -13,6 +15,7 @@ import watminwdi6e3s13.zad3.service.UserInfoService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/info")
@@ -38,5 +41,21 @@ public class InfoController {
         AccessToken accessToken = (AccessToken) request.getSession().getAttribute("accessToken");
         return userInfoService.getUserInfo(accessToken);
     }
+
+    @GetMapping(value = "/hashtag_cloud", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<HashtagEntity[]> getTagsFromTweets(HttpServletRequest request, @RequestParam(name = "tweets_number", defaultValue = "20") Integer tweetsNumber) throws TwitterException {
+        System.out.println(tweetsNumber);
+        AccessToken accessToken = (AccessToken) request.getSession().getAttribute("accessToken");
+        return userInfoService.getTagsFromTweets(tweetsNumber, accessToken);
+    }
+
+//    @GetMapping(value = "/tweet", produces = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseBody
+//    public List<HashtagEntity[]> getTagsFromTweets(HttpServletRequest request) throws TwitterException {
+////        AccessToken accessToken = (AccessToken) request.getSession().getAttribute("accessToken");
+////        List<HashtagEntity[]> hashArray = userInfoService.getTagsFromTweets(tweetsNumber, accessToken);
+////        return hashArray;
+//    }
 
 }
--- lab3/66183/src/main/java/watminwdi6e3s13/zad3/service/UserInfoService.java
+++ lab3/66183/src/main/java/watminwdi6e3s13/zad3/service/UserInfoService.java
@@ -3,11 +3,13 @@ package watminwdi6e3s13.zad3.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.User;
+import twitter4j.*;
 import twitter4j.auth.AccessToken;
 import watminwdi6e3s13.zad3.pojo.TwitterUser;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class UserInfoService {
@@ -31,10 +33,27 @@ public class UserInfoService {
                 .setDescription(user.getDescription())
                 .setEmail(user.getEmail())
                 .setFollowersNumber(user.getFollowersCount())
+                .setProfileImageURL(user.getOriginalProfileImageURL())
                 .setProfileURL(user.getURL())
                 .setScreenName(user.getScreenName());
 
         return twitterUser;
+    }
+
+    public List<HashtagEntity[]> getTagsFromTweets(Integer tweetsNumber, AccessToken accessToken) throws TwitterException {
+        myTwitter.setOAuthAccessToken(accessToken);
+        Stream<Status> statuses = myTwitter.timelines().getUserTimeline(new Paging(1, tweetsNumber)).stream();
+
+        return statuses.map(EntitySupport::getHashtagEntities).collect(Collectors.toList());
+
+
+    }
+
+    public void getFirstAndLastTweets(AccessToken accessToken) {
+//        myTwitter.setOAuthAccessToken(accessToken);
+//        myTwitter.getUserTimeline().
+
+        //TODO:// get last and first tweet
     }
 
 
