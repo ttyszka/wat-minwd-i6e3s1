fe2e5dafa841daeb2689426008df42b22cc00103
mknbl <pironi27@gmail.coom>
2019-12-09 22:01:54 +0100

    Initial commit

---
+++ lab2/59894/FlashscoreScraper/FlashscoreScraper/TableRow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashscoreScraper
+{
+    public class TableRow
+    {
+        public String position { get; set; }
+        public String name { get; set; }
+        public String matchesCount { get; set; }
+        public String wins { get; set; }
+        public String draws { get; set; }
+        public String losses { get; set; }
+        public String goals { get; set; }
+        public String points { get; set; }
+        
+    }
+}
